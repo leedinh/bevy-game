98bdf395e1b930f44f209e1894d760420aa691ee
l33dinh <45190091+leedinh@users.noreply.github.com>
2023-06-28 00:05:36 +0700

    systems chain example

--- bevy-ball-game/src/player/mod.rs
+++ bevy-ball-game/src/player/mod.rs
@@ -10,9 +10,13 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
    fn build(&self, app: &mut App) {
-       app.add_startup_system(spawn_player)
-        .add_system(player_movement)
-        .add_system(confine_player_movement)
+       app
+       .add_startup_system(spawn_player)
+        // .add_system(player_movement)
+        // .add_system(confine_player_movement.after(player_movement))
+        .add_systems((
+            player_movement,
+            confine_player_movement).chain())
         .add_system(enemy_hit_player)
         .add_system(player_hit_star);
    }
