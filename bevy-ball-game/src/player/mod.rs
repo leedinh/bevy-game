3877b3b453424c083d57f68da9c524109f53e4ac
l33dinh <45190091+leedinh@users.noreply.github.com>
2023-06-28 00:07:12 +0700

    Basic system set example

--- bevy-ball-game/src/player/mod.rs
+++ bevy-ball-game/src/player/mod.rs
@@ -6,17 +6,21 @@ mod systems;
 
 use systems::*;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
    fn build(&self, app: &mut App) {
        app
+            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
        .add_startup_system(spawn_player)
-        // .add_system(player_movement)
-        // .add_system(confine_player_movement.after(player_movement))
-        .add_systems((
-            player_movement,
-            confine_player_movement).chain())
+        .add_system(player_movement.in_set(MovementSystemSet))
+        .add_system(confine_player_movement.in_set(ConfinementSystemSet))
         .add_system(enemy_hit_player)
         .add_system(player_hit_star);
    }
