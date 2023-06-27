4dd63d1cfc7f433efabd5d95077356eac6cf00ca
l33dinh <45190091+leedinh@users.noreply.github.com>
2023-06-28 00:09:05 +0700

    enum systemset example

--- bevy-ball-game/src/player/mod.rs
+++ bevy-ball-game/src/player/mod.rs
@@ -6,21 +6,27 @@ mod systems;
 
 use systems::*;
 
-#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-pub struct MovementSystemSet;
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct MovementSystemSet;
+
+// #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+// pub struct ConfinementSystemSet;
 
 #[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
-pub struct ConfinementSystemSet;
+pub enum PlayerSystemSet {
+    Movement,
+    Confinement
+}
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
    fn build(&self, app: &mut App) {
        app
-            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
+            .configure_set(PlayerSystemSet::Movement.before(PlayerSystemSet::Confinement))
        .add_startup_system(spawn_player)
-        .add_system(player_movement.in_set(MovementSystemSet))
-        .add_system(confine_player_movement.in_set(ConfinementSystemSet))
+        .add_system(player_movement.in_set(PlayerSystemSet::Movement))
+        .add_system(confine_player_movement.in_set(PlayerSystemSet::Confinement))
         .add_system(enemy_hit_player)
         .add_system(player_hit_star);
    }
